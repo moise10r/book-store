bdb97926722a909dbd6a1660785bfb26ae1dc93d
moise10r <moiserushanika2006@gmail.com>
2021-08-23 11:04:06 +0200

    Add the header layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    Header
+    <div className="log-wrapper">
+      <span className="logo">Bookstore CMS</span>
+    </div>
+    <nav className="main-nav-bar">
+      <ul className="link-list">
+        <li className="link"><a href="#2">Books</a></li>
+        <li className="link"><a href="#2">Categories</a></li>
+      </ul>
+    </nav>
   </header>
 );
 
