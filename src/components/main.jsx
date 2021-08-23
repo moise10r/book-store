8040a37351c8da15311cea465b86681512e204d1
moise10r <moiserushanika2006@gmail.com>
2021-08-23 19:11:36 +0200

    Add Book collection

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,10 +2,43 @@ import React from 'react';
 import BookList from './bookList';
 import Header from './header';
 
+const books = [
+  {
+    id: 1,
+    category: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Chapter 17',
+      completed: '64',
+    },
+  },
+  {
+    id: 2,
+    category: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    progress: {
+      currentChapter: 'Chapter 3: "A Lesson Learned"',
+      completed: '8',
+    },
+  },
+  {
+    id: 3,
+    category: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Introduction',
+      completed: '0',
+    },
+  },
+];
+
 const Main = () => (
   <div className="container">
     <Header />
-    <BookList />
+    <BookList books={books} />
   </div>
 );
 
