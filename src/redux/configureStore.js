2cb178c9d5788c5cea8eef39df86849c5e5e75d6
moise10r <moiserushanika2006@gmail.com>
2021-08-24 23:01:54 +0200

    combine middlewares

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
 });
 
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk),
-);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
