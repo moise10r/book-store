2b3ff0ac6ce350cc4f6813962cb6e359571405e8
moise10r <moiserushanika2006@gmail.com>
2021-08-24 22:35:28 +0200

    Change actions type

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,42 +1,61 @@
-const ADD_BOOK = 'bookStore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOK_REQUEST = 'FETCH_BOOK_REQUEST';
+const FECTH_BOOK_SUCCESS = 'FETCH_BOOK_SUCCESS';
+const FECTH_BOOK_FAILURE = 'FECTH_BOOK_FAILURE';
 
-const initialState = [];
+// const initialState = {
+//   loading: false,
+//   books: [],
+//   error: '',
+// };
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
+// export const fecthBookRequest = () => ({
+//   type: FETCH_BOOK_REQUEST,
+// });
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  payload: {
-    id,
-  },
-});
+// export const fecthBookSuccess = (books) => ({
+//   type: FECTH_BOOK_SUCCESS,
+//   payload: books,
+// });
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return [
-        ...state,
-        {
-          id: action.payload.id,
-          category: action.payload.category,
-          title: action.payload.title,
-          author: action.payload.author,
-          progress: {
-            completPercentage: '0',
-            status: 'completed',
-          },
-          currentChapter: 'Introduction',
-        },
-      ];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload.id);
-    default:
-      return state;
-  }
-};
+// export const fecthBookFailure = (error) => ({
+//   type: FECTH_BOOK_FAILURE,
+//   payload: error,
+// });
+
+// export const addBook = (payload) => ({
+//   type: ADD_BOOK,
+//   payload,
+// });
+
+// export const removeBook = (id) => ({
+//   type: REMOVE_BOOK,
+//   payload: {
+//     id,
+//   },
+// });
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case FETCH_BOOK_REQUEST:
+//       return {
+//         ...state,
+//         loading: true,
+//       };
+//     case FECTH_BOOK_SUCCESS:
+//       return {
+//         loading: false,
+//         books: action.payload,
+//         error: '',
+//       };
+//     case FECTH_BOOK_FAILURE:
+//       return {
+//         loading: false,
+//         books: [],
+//         error: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
 export default reducer;
